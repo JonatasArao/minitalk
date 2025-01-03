287bbe817c2e887acd08588587ca753cadde6b40
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-01-03 09:39:35 -0300

    client - improving pid validation

--- mandatory/inc/minitalk.h
+++ mandatory/inc/minitalk.h
@@ -6,13 +6,14 @@
 /*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:03:07 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/02 17:32:59 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/03 09:22:14 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 # include <signal.h>
+# include <sys/types.h>
 # include "libft.h"
 
 #endif
--- mandatory/src_client/main.c
+++ mandatory/src_client/main.c
@@ -6,33 +6,39 @@
 /*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:03:03 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/02 17:39:38 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/03 09:36:20 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int	get_server_pid(int argc, char **argv)
+static pid_t	get_server_pid(int argc, char **argv)
 {
-	pid_t	server_pid;
+	long	server_pid;
+	char	*endptr;
 
 	if (argc != 3)
 	{
 		ft_putendl_fd("Usage : ./client <server_pid> <string_to_send>", 2);
-		return (0);
+		return (-1);
 	}
 	if (!ft_strall(argv[1], ft_isdigit))
 	{
-		ft_putendl_fd("Error: PID must be a valid value.", 2);
-		return (0);
+		ft_putendl_fd("Error: Server PID must contain only digits.", 2);
+		return (-1);
 	}
-	server_pid = ft_atoi(argv[1]);
-	if (kill(server_pid, SIGUSR1) == -1 || kill(server_pid, SIGUSR2) == -1)
+	server_pid = ft_strtol(argv[1], &endptr, 10);
+	if (server_pid > INT_MAX || *endptr != '\0')
+	{
+		ft_putendl_fd("Error: Invalid Server PID.", 2);
+		return (-1);
+	}
+	if (kill(server_pid, SIGUSR1) == -1)
 	{
 		ft_putendl_fd("Error: No such process or permission denied.", 2);
-		return (0);
+		return (-1);
 	}
-	return (server_pid);
+	return ((pid_t) server_pid);
 }
 
 int	main(int argc, char **argv)
@@ -40,7 +46,7 @@ int	main(int argc, char **argv)
 	pid_t	server_pid;
 
 	server_pid = get_server_pid(argc, argv);
-	if (!server_pid)
+	if (server_pid == -1)
 		return (1);
 	ft_putendl_fd("Minitalk Client", 1);
 	return (0);
