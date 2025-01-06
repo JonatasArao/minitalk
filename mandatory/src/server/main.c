9ddac2f39bbcc496b5641028200594bdf581d4b1
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-01-06 14:50:46 -0300

    server - receive_char_signal now print received char (still unstable)

--- mandatory/src/server/main.c
+++ mandatory/src/server/main.c
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:04:37 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/03 15:09:06 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/06 13:38:52 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,35 @@
 
 void	receive_char_signal(int sig)
 {
+	static char	c = 0;
+	static int	bit_count = 0;
+
 	if (sig == SIGUSR1)
-		ft_putnbr_fd(0, 1);
+		c &= ~(1 << bit_count);
 	else if (sig == SIGUSR2)
-		ft_putnbr_fd(1, 1);
+		c |= (1 << bit_count);
+	bit_count++;
+	if (bit_count == 8)
+	{
+		ft_putchar_fd(c, 1);
+		c = 0;
+		bit_count = 0;
+	}
 }
 
 int	main(void)
 {
 	pid_t				server_pid;
-	struct sigaction	s_sa;
+	struct sigaction	s_action;
 
 	server_pid = getpid();
 	ft_putnbr_fd(server_pid, 1);
 	ft_putchar_fd('\n', 1);
-	s_sa.sa_handler = receive_char_signal;
-	sigemptyset(&s_sa.sa_mask);
-	s_sa.sa_flags = 0;
-	sigaction(SIGUSR1, &s_sa, NULL);
-	sigaction(SIGUSR2, &s_sa, NULL);
+	s_action.sa_handler = receive_char_signal;
+	sigemptyset(&s_action.sa_mask);
+	s_action.sa_flags = 0;
+	sigaction(SIGUSR1, &s_action, NULL);
+	sigaction(SIGUSR2, &s_action, NULL);
 	while (1)
 	{
 		pause();
