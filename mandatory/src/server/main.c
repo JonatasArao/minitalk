8c6365817851059abf455dc09069c286ffce1570
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-01-07 15:21:44 -0300

    server - now echoes every bit to client

--- mandatory/src/server/main.c
+++ mandatory/src/server/main.c
@@ -6,21 +6,46 @@
 /*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:04:37 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/07 08:22:23 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/07 15:17:40 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	collect_and_print_string(char c)
+{
+	static char	*string = NULL;
+	char		c_string[2];
+	char		*temp;
+
+	if (!string)
+		string = ft_strdup("");
+	if (c && string)
+	{
+		c_string[0] = c;
+		c_string[1] = '\0';
+		temp = ft_strjoin(string, c_string);
+		if (!temp)
+			ft_delpointer((void **)&string);
+		free(string);
+		string = temp;
+	}
+	else
+	{
+		if (*string)
+			ft_putendl_fd(string, 1);
+		ft_delpointer((void **)&string);
+	}
+}
+
 void	receive_char_signal(int sig, siginfo_t *info, void *context)
 {
 	static char		c = 0;
 	static int		bit_count = 0;
-	static pid_t	client_pid = 0;
+	pid_t			client_pid;
 
 	(void) context;
-	if (info->si_pid)
-		client_pid = info->si_pid;
+	client_pid = info->si_pid;
 	if (sig == SIGUSR1)
 		c &= ~(1 << bit_count);
 	else if (sig == SIGUSR2)
@@ -28,13 +53,17 @@ void	receive_char_signal(int sig, siginfo_t *info, void *context)
 	bit_count++;
 	if (bit_count == 8)
 	{
-		ft_putchar_fd(c, 1);
-		if (c == '\0')
-			ft_putchar_fd('\n', 1);
+		collect_and_print_string(c);
 		c = 0;
 		bit_count = 0;
 	}
-	kill(client_pid, sig);
+	if (kill(client_pid, sig) == -1)
+	{
+		collect_and_print_string('\0');
+		ft_putendl_fd("Error: client disconnected.", 2);
+		c = 0;
+		bit_count = 0;
+	}
 }
 
 int	main(void)
@@ -43,6 +72,7 @@ int	main(void)
 	t_sigaction	action;
 
 	server_pid = getpid();
+	ft_putstr_fd("Minitalk Server - PID: ", 1);
 	ft_putnbr_fd(server_pid, 1);
 	ft_putchar_fd('\n', 1);
 	action.sa_sigaction = receive_char_signal;
@@ -52,5 +82,5 @@ int	main(void)
 	sigaction(SIGUSR2, &action, NULL);
 	while (1)
 		pause();
-	return (0);
+	return (EXIT_SUCCESS);
 }
