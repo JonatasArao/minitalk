356cafe3dd1a0eaff85d5287630c8ab091a939ce
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-01-07 15:22:41 -0300

    header - avoiding trouble

--- mandatory/inc/minitalk.h
+++ mandatory/inc/minitalk.h
@@ -6,15 +6,12 @@
 /*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:03:07 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/07 07:11:26 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/07 12:57:13 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
-# ifdef __linux__
-#  define _GNU_SOURCE
-# endif
 # include <signal.h>
 # include <sys/types.h>
 # include "libft.h"
