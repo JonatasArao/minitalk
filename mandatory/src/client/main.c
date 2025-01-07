375e6b50f0760ac3acfd44c37d82a26cc88c5d96
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-01-07 15:21:16 -0300

    client - server timeout and acknowledgment improved

--- mandatory/src/client/main.c
+++ mandatory/src/client/main.c
@@ -6,13 +6,13 @@
 /*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:03:03 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/07 08:36:42 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/07 14:19:59 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static volatile int	g_acknowledgment = 0;
+static volatile sig_atomic_t	g_echo = 0;
 
 static pid_t	get_server_pid(int argc, char **argv)
 {
@@ -22,26 +22,40 @@ static pid_t	get_server_pid(int argc, char **argv)
 	if (argc != 3)
 	{
 		ft_putendl_fd("Usage : ./client <server_pid> <string_to_send>", 2);
-		return (-1);
+		exit(EXIT_FAILURE);
 	}
 	if (!ft_strall(argv[1], ft_isdigit))
 	{
 		ft_putendl_fd("Error: Server PID must contain only digits.", 2);
-		return (-1);
+		exit(EXIT_FAILURE);
 	}
 	server_pid = ft_strtol(argv[1], &endptr, 10);
 	if (server_pid > INT_MAX || *endptr != '\0')
 	{
 		ft_putendl_fd("Error: Invalid Server PID.", 2);
-		return (-1);
+		exit(EXIT_FAILURE);
 	}
 	return ((pid_t) server_pid);
 }
 
-void	receive_confirmation_response(int sig)
+void	receive_server_response(int sig)
 {
-	(void) sig;
-	g_acknowledgment = 1;
+	if (sig == SIGUSR1 || sig == SIGUSR2)
+		g_echo = sig;
+}
+
+int	timeout_server(int timeout)
+{
+	while (!g_echo && timeout > 0)
+	{
+		usleep(250);
+		timeout -= 250;
+	}
+	if (timeout <= 0)
+		return (-1);
+	if (g_echo)
+		g_echo = 0;
+	return (0);
 }
 
 int	send_char_signal(char c, pid_t server_pid)
@@ -62,22 +76,12 @@ int	send_char_signal(char c, pid_t server_pid)
 			return (-1);
 		}
 		bit_count++;
-		while (!g_acknowledgment)
-			;
-		if (g_acknowledgment)
-			g_acknowledgment = 0;
-	}
-	return (0);
-}
-
-int	send_string_signal(char *str, pid_t server_pid)
-{
-	while (*str)
-	{
-		if (send_char_signal(*str++, server_pid) == -1)
+		if (timeout_server(500000) == -1)
+		{
+			ft_putendl_fd("Error: Server response timeout.", 2);
 			return (-1);
+		}
 	}
-	send_char_signal('\0', server_pid);
 	return (0);
 }
 
@@ -86,14 +90,20 @@ int	main(int argc, char **argv)
 	pid_t		server_pid;
 	t_sigaction	action;
 
+	ft_putstr_fd("Minitalk Client - ", 1);
 	server_pid = get_server_pid(argc, argv);
-	if (server_pid == -1)
-		return (1);
-	action.sa_handler = receive_confirmation_response;
+	action.sa_handler = receive_server_response;
 	sigemptyset(&action.sa_mask);
 	action.sa_flags = 0;
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
-	ft_putendl_fd("Minitalk Client", 1);
-	return (send_string_signal(argv[2], server_pid));
+	while (*argv[2])
+	{
+		if (send_char_signal(*argv[2]++, server_pid) == -1)
+			return (EXIT_FAILURE);
+	}
+	if (send_char_signal('\0', server_pid) == -1)
+		return (EXIT_FAILURE);
+	ft_putendl_fd("Message sent successfully.", 1);
+	return (EXIT_SUCCESS);
 }
