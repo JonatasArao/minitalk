4acb2573d1aa6e3301d015908de4213c294f7bc7
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-01-07 08:34:47 -0300

    header - typedef for sigaction and linux support (prevent VScode error)

--- mandatory/inc/minitalk.h
+++ mandatory/inc/minitalk.h
@@ -6,14 +6,19 @@
 /*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:03:07 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/03 15:09:01 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/07 07:11:26 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
+# ifdef __linux__
+#  define _GNU_SOURCE
+# endif
 # include <signal.h>
 # include <sys/types.h>
 # include "libft.h"
+
+typedef struct sigaction	t_sigaction;
 
 #endif
